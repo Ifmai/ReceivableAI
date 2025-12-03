11800d370ed9dc3b5561ca3eb37089c032def339
Ifmai <alpozdemir044@gmail.com>
2025-12-03 19:16:12 +0000

    choose: isim değişiklikleri yapıldı.

--- collection_project/customers/models.py
+++ collection_project/customers/models.py
@@ -4,11 +4,11 @@ from django.db import models
 class Customer (models.Model):
 	name = models.CharField(max_length=50, null=False)
 	tax_number = models.CharField(max_length=100, null=False, unique=True)
-	e_mail = models.EmailField(unique=True)
-	phone = models.IntegerField(blank=True, unique=True)
-	status = models.BooleanField()
-	created_time = models.DateTimeField(auto_now_add=True)
-	update_time = models.DateTimeField(auto_now=True)
+	e_mail = models.EmailField(null=False)
+	phone = models.DecimalField(blank=True, max_digits=11, decimal_places=0)
+	is_active = models.BooleanField(default=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
 
 
 	def __str__(self):
