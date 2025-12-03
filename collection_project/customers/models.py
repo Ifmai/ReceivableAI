85b39a167d0f62e81af74d8e57fde6c13e8b7e3f
Ifmai <alpozdemir044@gmail.com>
2025-12-03 13:46:21 +0000

    feat: customer modeli eklendi.

--- collection_project/customers/models.py
+++ collection_project/customers/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer (models.Model):
+	name = models.CharField(max_length=50, null=False)
+	tax_number = models.CharField(max_length=100, null=False, unique=True)
+	e_mail = models.EmailField(unique=True)
+	phone = models.IntegerField(blank=True, unique=True)
+	status = models.BooleanField()
+	created_time = models.DateTimeField(auto_now_add=True)
+	update_time = models.DateTimeField(auto_now=True)
+
+
+	def __str__(self):
+		return f'{self.name}'
+	class Meta:
+		db_table = 'customers_customer'
+
