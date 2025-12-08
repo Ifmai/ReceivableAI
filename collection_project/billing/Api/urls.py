abad8c47bd3dde55f4319e1f06e073836d79cd9e
Ifmai <alpozdemir044@gmail.com>
2025-12-08 14:15:23 +0000

    refactor: yeni python düzenleyici eklendi. ona göre refactor edildi.

--- collection_project/billing/Api/urls.py
+++ collection_project/billing/Api/urls.py
@@ -3,22 +3,19 @@ from .Invoice.views import *
 from .Payments.views import *
 
 urlpatterns = [
-	#Invoice Urls Start
-	path("invoices/add/", InvoiceCreateView.as_view(), name='invoice_create'),
-	path("invoices/list/", InvoiceListView.as_view(), name='invoice_list'),
-	path("invoices/upcoming/", InvoiceUpcomingView.as_view(), name='invoice_upcoming'),
-	path("invoices/overdue/", InvoiceOverDueView.as_view(), name='invoice_overdue'),
-	path("invoices/detail/<str:encrypted_id>/", InvoiceDetailView.as_view(), name='invoice_detail'),
-	#Invoice Urls End
-
-	#Payment Urls Start	
-	path("payments/add/", PaymentCreateView.as_view(), name='Payment_create'),
-	path("payments/list/", PaymentListView.as_view(), name='Payment_list'),
-	path("payments/detail/<str:encrypted_id>/", PaymentDetailView.as_view(), name='payment_detail'),
-	#Payment Urls End
-
-
-	#Integration Urls (n8n vb. için) Start
-	path("integrations/", include("billing.Api.N8N.urls")),
-	#Integration Urls (n8n vb. için) End
+    # Invoice Urls Start
+    path("invoices/add/", InvoiceCreateView.as_view(), name="invoice_create"),
+    path("invoices/list/", InvoiceListView.as_view(), name="invoice_list"),
+    path("invoices/upcoming/", InvoiceUpcomingView.as_view(), name="invoice_upcoming"),
+    path("invoices/overdue/", InvoiceOverDueView.as_view(), name="invoice_overdue"),
+    path("invoices/detail/<str:encrypted_id>/", InvoiceDetailView.as_view(), name="invoice_detail"),
+    # Invoice Urls End
+    # Payment Urls Start
+    path("payments/add/", PaymentCreateView.as_view(), name="Payment_create"),
+    path("payments/list/", PaymentListView.as_view(), name="Payment_list"),
+    path("payments/detail/<str:encrypted_id>/", PaymentDetailView.as_view(), name="payment_detail"),
+    # Payment Urls End
+    # Integration Urls (n8n vb. için) Start
+    path("integrations/", include("billing.Api.N8N.urls")),
+    # Integration Urls (n8n vb. için) End
 ]
